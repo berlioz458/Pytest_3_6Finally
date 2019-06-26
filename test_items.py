2bf56c6c2eb78d6e29672b01f584adf3349a5b41
shulinina.e <test-2@prodv.net>
2019-06-26 12:06:55 +0700

    Autotest for multi-language (for Chrome)

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_button_add(browser):
     browser.get(link)
     browser.find_element_by_css_selector("button.btn-add-to-basket")
